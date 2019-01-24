c10a56d2d3bac42c3c0bb01a594510efff452e7b
Donovan C. Haines <donovan@drhaines.org>
2019-01-23 22:06:01 -0600

    Initial Upload

--- P450Titration/P450Titration.py
+++ P450Titration/P450Titration.py
@@ -1,1 +1,78 @@
+from tkinter import *  #to use graphical user interface (windowed program)
+from tkinter import filedialog
+import pandas
+from pandastable import Table, TableModel
+
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+
+import spectra
+
+class MyGUI:
+    def __init__(self,master):
+      self.master = master
+      master.title("P450 Titration Analysis")
+      master.geometry('1920x1080')
+
+      
+      #self.greet_button = Button(master, text="Greet", command=self.greet)
+      #self.greet_button.pack()
+      
+      #self.close_button = Button(master, text="Close", command=master.destroy)
+      #self.close_button.pack()
+
+      menu=Menu(master)
+      new_item=Menu(menu)
+      new_item.add_command(label="Load Spectra", command=self.load_spectra)
+      new_item.add_command(label="Load Reference Spectra", command=self.load_reference)
+      new_item.add_command(label="Exit", command=master.destroy)
+      menu.add_cascade(label='File', menu=new_item)
+      master.config(menu=menu)
+      
+    
+    def greet(self):
+      print("Greetings!")
+    
+    def load_spectra(self):
+      loaded_spectra = spectra.spectral_collection(load=TRUE)
+      print(loaded_spectra)
+
+      f1=Frame(self.master)
+      f1.pack(side = LEFT, fill=BOTH,expand=1)
+      df = loaded_spectra.df
+      pt = Table(f1, dataframe=df,
+                                    showtoolbar=True, showstatusbar=True)
+      pt.show()
+
+      f2 = Frame(self.master)
+      f2.pack(side = RIGHT, fill = BOTH, expand=1)
+
+      myfig = plt.Figure(figsize=(5,5), dpi=100)
+      aplot = myfig.add_subplot(111)
+      aplot.plot(loaded_spectra.spectra[0].wavelengths,loaded_spectra.spectra[0].absorbance)
+      for count in range(1,loaded_spectra.number_spectra):
+          aplot.plot(loaded_spectra.spectra[count].wavelengths,loaded_spectra.spectra[count].absorbance)
+
+      canvas = FigureCanvasTkAgg(myfig, f2)
+      canvas.draw()
+      canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=TRUE)
+      toolbar = NavigationToolbar2Tk(canvas, f2)
+      toolbar.update()
+      canvas._tkcanvas.pack(side=TOP, fill=BOTH, expand=TRUE)
+
+
+    def load_reference(self):
+      self.ref_filename = filedialog.askopenfilename()
+      print(self.ref_filename)
+      self.ref_data=pandas.read_csv(self.ref_filename, header=None)
+
+
+
+root = Tk()
+my_gui = MyGUI(root)
+root.mainloop()
+
+
 
---
+++ P450Titration/spectra.py
@@ -0,0 +1,123 @@
+import pandas
+from tkinter import *
+
+import numpy as np
+
+######################################################################
+######################################################################
+######################################################################
+######################################################################
+######################################################################
+######################################################################
+class uvvis_spectrum:
+  def __init__(self):
+    self.wl_low=-1.0
+    self.wl_high=-1.0
+    self.filename=""
+    self.sample_name=""
+    self.collection_time=""
+    self.instrument=""
+    self.avg_time=0.0
+    self.baseline_method=""
+    self.comments=""
+    self.wavelength=list()
+    self.absorbance=list()
+  
+    ####################################################################
+  # borrowed from https://stackoverflow.com/questions/1535327/how-to-print-objects-of-class-using-print
+  def __str__(self):
+    return str(self.__class__) + ": " + str(self.__dict__)   
+
+
+
+######################################################################
+######################################################################
+######################################################################
+######################################################################
+######################################################################
+######################################################################
+class spectral_collection:
+  def __init__(self, filename="", load=FALSE):
+    self.number_spectra=0
+    self.number_files_loaded=0
+    self.spectra=list()
+    self.filenames=list()
+    self.rawdata=list()
+    self.df = pandas.DataFrame()
+    if load : self.load(filename)
+  
+  ####################################################################
+  # borrowed from https://stackoverflow.com/questions/1535327/how-to-print-objects-of-class-using-print
+  def __str__(self):
+    return str(self.__class__) + ": " + str(self.__dict__) 
+    
+  ####################################################################
+  def load(self, filename="", generatedf=True):
+    #load a csv file from the Cary50
+    if filename=="": 
+      filename = filedialog.askopenfilename()
+    print(filename)
+    
+    #Add filename to list of filenames (because can import more files later)
+    self.filenames.append(filename)
+    self.rawdata.append(pandas.read_csv(filename, header=None))
+    self.number_files_loaded = self.number_files_loaded + 1
+
+
+    names=self.rawdata[self.number_files_loaded-1].iloc[0]
+    labels=self.rawdata[self.number_files_loaded-1].iloc[1]
+    print(names)
+    print(labels)
+
+    # Determine number of valid wavelength, Abs rows
+    num_wavelength=0
+    for count in range(self.rawdata[self.number_files_loaded-1].shape[0]):
+      try: #test first to columns to see if they are floats
+        #print("Cycle {}".format(count))
+        #print(self.rawdata[self.number_files_loaded-1].loc[count,0:1])
+        float(self.rawdata[self.number_files_loaded-1].loc[count,0])
+        float(self.rawdata[self.number_files_loaded-1].loc[count,1])
+        num_wavelength = num_wavelength + 1
+        #print("Count {} : yes".format(count))
+      except:
+        print("Count {} : not a float pair row.".format(count))
+    print("Found {} rows of float pair data.".format(num_wavelength))
+
+    # for each spectrum
+    for count in range((self.rawdata[self.number_files_loaded-1].shape[1])//2):
+      #print("Spectral loop count = {}".format(count))
+      this_spectrum = uvvis_spectrum()
+      this_spectrum.filename = filename
+      #this_spectrum.wl_low = min()
+      xvalues=pandas.to_numeric(self.rawdata[self.number_files_loaded-1].loc[2:num_wavelength+1, count*2])
+      yvalues=pandas.to_numeric(self.rawdata[self.number_files_loaded-1].loc[2:num_wavelength+1, (count*2)+1])
+      this_spectrum.wavelengths = xvalues
+      this_spectrum.absorbance = yvalues
+      this_spectrum.sample_name=(self.rawdata[self.number_files_loaded-1].loc[0, (count*2)+1])
+
+      self.spectra.append(this_spectrum)
+      self.number_spectra = self.number_spectra+1
+
+      if generatedf:
+        xvalues.name="WL:"+names[count*2]
+        yvalues.name="AU:"+names[count*2]
+        #print(type(xvalues))
+        
+        tempdf = pandas.concat([xvalues,yvalues], axis=1, ignore_index=True)
+        tempdf.columns=[xvalues.name, yvalues.name] # not sure why this is necessary, but seems to be
+        #print(tempdf)
+        if count==0:
+          self.df=tempdf
+        else:
+          self.df = pandas.concat([self.df,tempdf], axis=1)
+          #print("Appended df")
+          #print("xvalues")
+          #print(xvalues[0:3])
+          #print("yvalues")
+          #print(yvalues[0:3])
+          #print("tempdf")
+          #print(tempdf.iloc[0:3,:])
+          #print("self.df:")
+          #print(self.df.iloc[0:3,:])
+        print("self.df in spectral collection ended up being:")
+        print(self.df)
