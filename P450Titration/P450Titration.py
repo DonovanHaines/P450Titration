d14eb9300c3b30f5c33d1ae4b6f3d023585a8e0e
Donovan Haines <donovan@drhaines.org>
2019-01-30 18:45:58 -0600

    Big fig window finished, now to code transfer of paramters to baseline fits

--- P450Titration/P450Titration.py
+++ P450Titration/P450Titration.py
@@ -50,11 +50,8 @@ class MyGUI:
       new_item3.add_command(label="About this program", command=self.about)
       menu.add_cascade(label="Help", menu=new_item3)
 
-
-    
-    def greet(self):
-      print("Greetings!")
-    
+          
+    ################################################################################################
     def load_spectra(self):
       self.loaded_spectra = loaded_spectra = spectra.spectral_collection(load=TRUE)
       print(loaded_spectra)
@@ -111,44 +108,206 @@ class MyGUI:
       toolbar = NavigationToolbar2Tk(canvas, f2)
       toolbar.update()
       canvas._tkcanvas.pack(side=TOP, fill=BOTH, expand=TRUE)
-
+    
+    ############################################################################
     def baseline_all(self):
         base_win=Toplevel(width=600, height=800) #make new popup window
-
-        use_bg = IntVar()
-        use_gauss = IntVar()
-        use_scatter = IntVar()
-        use_ref1 = IntVar()
-        use_ref2 = IntVar()
-        use_ref3 = IntVar()
+        base_win.rowconfigure(0, weight=0, minsize=300)
+        base_win.rowconfigure(1, weight=1, minsize=300)
+        base_win.columnconfigure(0, weight=0, minsize=300)
+        base_win.columnconfigure(1, weight=1, minsize=300)
+        bv_use_bg = IntVar()
+        bv_use_gauss = IntVar()
+        bv_use_scatter = IntVar()
+        bv_use_ref1 = IntVar()
+        bv_use_ref2 = IntVar()
+        bv_use_ref3 = IntVar()
         select_var = StringVar()
+        bv_run_all = IntVar()
 
         selection_frame = Frame(base_win)
-
+        selection_frame.grid(padx=30, pady=30)
         #sample_names=list()
         #for this_spectrum in self.loaded_spectra.spectra:
         #    sample_names.append(this_spectrum.sample_name)
+        
+        #Create dropdown box listing all spectra available (non-ref)
+        w_opt=self.loaded_spectra.sample_names
+        select_var.set(w_opt[0])
+        w = OptionMenu(selection_frame,select_var, *w_opt)
+        #w = OptionMenu(selection_frame, select_var, self.loaded_spectra.sample_names)
+        num_selection_columns=4
+        w.grid(columnspan=num_selection_columns)
 
-        w = OptionMenu(selection_frame, select_var, self.loaded_spectra.sample_names)
-        w.pack()
+        Label(selection_frame, text="Constant?").grid(columnspan=2, row = 1, column = 3)
 
-        b1 = Checkbutton(selection_frame, text="Constant Background", variable = use_bg)
-        b1.pack()
-        b2 = Checkbutton(selection_frame, text="Nearby Gaussian Background", variable = use_gauss)
-        b2.pack()
-        b3 = Checkbutton(selection_frame, text="Scattering Background", variable = use_scatter)
-        b3.pack()
-        b4 = Checkbutton(selection_frame, text="Reference #1", variable = use_ref1)
-        b4.pack()
-        b5 = Checkbutton(selection_frame, text="Reference #2", variable = use_ref2)
-        b5.pack()
-        b6 = Checkbutton(selection_frame, text="Reference #3", variable = use_ref3)
-        b6.pack()
+        b1 = Checkbutton(selection_frame, text="Constant Background", variable = bv_use_bg)
+        b1.grid(row=3, sticky=W)
+        
+        l1 = Label(selection_frame, text="Initial constant bg value:")
+        l1.grid(row=4, column=1, sticky=W)
+        e1 = Entry(selection_frame)
+        e1.grid(row = 4, column = 2, sticky=W)
+        bv_b1a = DoubleVar()
+        b1a = Checkbutton(selection_frame, variable = bv_b1a)
+        b1a.grid(row=4, column = 3, sticky=W, padx=20)
 
-        butt_ok = Button(selection_frame, text="Run", command = lambda: self.baseline())
-        butt_ok.pack()
 
-        selection_frame.pack()
+        b2 = Checkbutton(selection_frame, text="Nearby Gaussian Background", variable = bv_use_gauss)
+        b2.grid(row = 6, sticky = W)
+        
+        l2a = Label(selection_frame, text="Initial amplitude:")
+        l2a.grid(row = 7, column=1, sticky = W)
+        e2a = Entry(selection_frame)
+        e2a.grid(row = 7, column = 2, sticky = W)
+        bv_b2a = DoubleVar()
+        b2a = Checkbutton(selection_frame, variable = bv_b2a)
+        b2a.grid(row=7, column = 3, sticky=W, padx=20)
+
+        l2b = Label(selection_frame, text="Initial width:")
+        l2b.grid(row = 8, column=1, sticky = W)
+        e2b = Entry(selection_frame)
+        e2b.grid(row = 8, column = 2, sticky = W)
+        bv_b2b = DoubleVar()
+        b2b = Checkbutton(selection_frame, variable = bv_b2b)
+        b2b.grid(row=8, column = 3, sticky=W, padx=20)
+
+        l2c = Label(selection_frame, text="Initial wavelength:")
+        l2c.grid(row = 9, column = 1, sticky = W)
+        e2c = Entry(selection_frame)
+        e2c.grid(row = 9, column = 2, sticky = W)
+        bv_b2c = DoubleVar()
+        b2c = Checkbutton(selection_frame, variable = bv_b2c)
+        b2c.grid(row=9, column = 3, sticky=W, padx=20)
+
+        b3 = Checkbutton(selection_frame, text="Scattering Background", variable = bv_use_scatter)
+        b3.grid(row=10, sticky=W)
+
+        l3a = Label(selection_frame, text="Initial amplitude:")
+        l3a.grid(row = 11, column=1, sticky = W)
+        e3a = Entry(selection_frame)
+        e3a.grid(row = 11, column = 2, sticky = W)
+        bv_b3a = DoubleVar()
+        b3a = Checkbutton(selection_frame, variable = bv_b3a)
+        b3a.grid(row=11, column = 3, sticky=W, padx=20)
+
+
+        l3b = Label(selection_frame, text="Initial proportionality:")
+        l3b.grid(row = 12, column = 1, sticky = W)
+        e3b = Entry(selection_frame)
+        e3b.grid(row = 12, column = 2, sticky = W)
+        bv_b3b = DoubleVar()
+        b3b = Checkbutton(selection_frame, variable = bv_b3b)
+        b3b.grid(row=12, column = 3, sticky=W, padx=20)
+
+        l3c = Label(selection_frame, text="Initial power:")
+        l3c.grid(row = 13, column = 1, sticky = W)
+        e3c = Entry(selection_frame)
+        e3c.grid(row = 13, column = 2, sticky = W)
+        bv_b3c = DoubleVar()
+        b3c = Checkbutton(selection_frame, variable = bv_b3c)
+        b3c.grid(row=13, column = 3, sticky=W, padx=20)
+
+        b4 = Checkbutton(selection_frame, text="Reference #1", variable = bv_use_ref1)
+        b4.grid(row=14, sticky=W)
+
+        l4a = Label(selection_frame, text="Initial multiplier:")
+        l4a.grid(row = 15, column=1, sticky = W)
+        e4a = Entry(selection_frame)
+        e4a.grid(row = 15, column = 2, sticky = W)
+        bv_b4a = DoubleVar()
+        b4a = Checkbutton(selection_frame, variable = bv_b4a)
+        b4a.grid(row=15, column = 3, sticky=W, padx=20)
+
+
+        b5 = Checkbutton(selection_frame, text="Reference #2", variable = bv_use_ref2)
+        b5.grid(row=16, sticky=W)
+
+        l5a = Label(selection_frame, text="Initial multiplier:")
+        l5a.grid(row = 17, column = 1, sticky = W)
+        e5a = Entry(selection_frame)
+        e5a.grid(row = 17, column = 2, sticky = W)
+        bv_b5a = DoubleVar()
+        b5a = Checkbutton(selection_frame, variable = bv_b5a)
+        b5a.grid(row=17, column = 3, sticky=W, padx=20)
+
+
+        b6 = Checkbutton(selection_frame, text="Reference #3", variable = bv_use_ref3)
+        b6.grid(row = 18, sticky=W)
+            
+        l6a = Label(selection_frame, text="Initial multiplier:")
+        l6a.grid(row = 19, column=1, sticky = W)
+        e6a = Entry(selection_frame)
+        e6a.grid(row = 19, column = 2, sticky = W)
+        bv_b7a = DoubleVar()
+        b7a = Checkbutton(selection_frame, variable = bv_b7a)
+        b7a.grid(row=19, column = 3, sticky=W, padx=20)
+
+        l8 = Label(selection_frame, text="Fit between wavelengths ")
+        l8.grid(row = 20, column = 0)
+        ent_wave1 = Entry(selection_frame)
+        ent_wave1.grid(row = 20, column = 1)
+        l8a = Label(selection_frame, text = " nm and ")
+        l8a.grid(row = 20, column = 2)
+        ent_wave2 = Entry(selection_frame)
+        ent_wave2.grid(row = 20, column = 3)
+        l8b = Label(selection_frame, text = " nm")
+        l8b.grid(row = 20, column = 4)
+
+        b_run_all = Checkbutton(selection_frame, text="Use same parameters and fit all", variable = bv_run_all)
+        b_run_all.grid(columnspan=num_selection_columns, row=25, padx=20, pady=30)
+
+        b_ok = Button(selection_frame, text="Run", command = lambda: self.baseline())
+        b_ok.grid(columnspan=num_selection_columns, row=26)
+
+        selection_frame.grid(column = 0, row =0, padx=30, pady=30)
+        
+        # now set to not shrink window too small for entry widgets
+        #base_win.update()
+        #temp=selection_frame.grid_info()
+        #rowconfigure(0, minsize=selection_frame.winfo_height)
+        #base_win.columnconfigure(0, minsize=selection_frame.winfo_width)
+
+
+        #########################################
+        #Now define results and graph pane
+        output_frame = Frame(base_win, height=100)
+        output_frame.grid(column = 0, row = 1, padx=20, pady=20, sticky='NSWE')
+        results_scrollbar = Scrollbar(output_frame)
+        results_scrollbar.grid(column = 2, sticky='NS')
+        textbuffer = Text(output_frame, yscrollcommand = results_scrollbar.set)
+        textbuffer.grid(column = 0, sticky='NSWE')
+        quote = """HAMLET: To be, or not to be--that is the question:
+        Whether 'tis nobler in the mind to suffer
+        The slings and arrows of outrageous fortune
+        Or to take arms against a sea of troubles
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+
+        And by opposing end them. To die, to sleep--
+        No more--and by a sleep to say we end
+        The heartache, and the thousand natural shocks
+        That flesh is heir to. 'Tis a consummation
+        Devoutly to be wished."""
+        textbuffer.insert(END, quote)
+
+        graph_frame = Frame(base_win, bg = "red", width=600, height=600)
+        graph_frame.grid(rowspan=2, column = 1, row = 0, padx=20, pady=20, sticky='NSWE')
 
 
     def baseline(self):
@@ -194,7 +353,7 @@ class MyGUI:
         return
 
     def about(self):
-        tkinter.messagebox.showinfo("About this program", "P450 Titration Analysis by Donovan C. Haines, Sam Houston State University")
+        tkinter.messagebox.showinfo("About this program", "P450 Titration Analysis by Donovan C. Haines, Sam Houston State University. Email:haines@shsu.edu.")
         return
 
 
